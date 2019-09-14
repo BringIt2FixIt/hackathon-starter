798d988bab1aef95c6e3242f1b650b96c63bd27f
mblackstock <mikeblackstock@gmail.com>
2019-09-14 14:19:16 -0700

    adding some categories to help form

--- controllers/help.js
+++ controllers/help.js
@@ -14,6 +14,7 @@ exports.index = (req, res) => {
 exports.sendHelp = (req, res, next) => {
   const validationErrors = [];
   if (validator.isEmpty(req.body.message)) validationErrors.push({ msg: 'Message cannot be blank.' });
+  if (validator.isEmpty(req.body.category)) validationErrors.push({ msg: 'You must select a category.' });
 
   if (validationErrors.length) {
     req.flash('errors', validationErrors);
@@ -22,10 +23,11 @@ exports.sendHelp = (req, res, next) => {
 
   // TODO: get the phone number for the right person from the database
   const message = {
-    to: '+16047609035', // always send to Mike for now
+    to: '+16047609035', // always send to Mike for now 
     from: '+17787711046',
-    body: req.body.message
+    body: req.user.profile.name+' needs help with ' +req.body.category+'! '+req.body.message
   };
+  
   twilio.messages.create(message).then((sentMessage) => {
     req.flash('success', { msg: `Text send to ${sentMessage.to}` });
     res.redirect('/api/twilio');
